a6100c9d7e2ab5b42081dd312248976a6733775c
wellingtondsf <wellington@bridge.ufsc.br>
2020-02-12 16:58:39 -0300

    criacao dos projetos

---
+++ backend/src/main/java/br/ufsc/avaliacaomunicipal/AvaliacaoMunicipalApplication.java
@@ -0,0 +1,13 @@
+package br.ufsc.avaliacaomunicipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoMunicipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoMunicipalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/ufsc/avaliacaomunicipal/AvaliacaoMunicipalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsc.avaliacaomunicipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoMunicipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
