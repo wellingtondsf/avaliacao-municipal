72fa2638ae82dc66c3a742b5048c34db9c5f5698
wellingtondsf <wellington@bridge.ufsc.br>
2020-12-05 14:39:06 -0300

    mensagem tela principal

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,12 +1,17 @@
 import { HFlow, VFlow } from "bold-ui";
 import React from "react";
 import { CustomLink } from "./Navegacao";
+import {Text} from "bold-ui"
 
 export const Home = () => {
     return (
         <VFlow>
-            <HFlow justifyContent='center' style={{fontSize: '10rem'}}>
-            <CustomLink label= 'Avaliar um município' link='/avaliar' size='large'/>
+            <HFlow justifyContent='center' >
+                <VFlow vSpacing={7} style={{alignItems: 'center', paddingTop:'10rem'}}>
+                <Text fontWeight= 'bold' fontSize= {1}>Auxiliar na avaliação municipal pode ajudar seu município a ter uma melhor disponibilização de informação, possibilitando o desenvolvimento de novos negócios com as 
+                informações disponibilizadas, além de viabilizar que seja auditado o que esta sendo feito com o seu dinheiro no município, e se o responsável pelo município esta fazendo o que prometeu.</Text>
+            <CustomLink label= 'Avaliar um município' link='/avaliar' size='large' kind='primary' style={{paddingTop: '5rem'}}/>
+            </VFlow>
             </HFlow>
         </VFlow>
     )
